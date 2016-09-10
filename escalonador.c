7408931284591fa8ae6eec29fff097e85630bad9
hassakura <helio.assakura@gmail.com>
2016-09-09 22:15:06 -0300

    comofas threads?

--- escalonador.c
+++ escalonador.c
@@ -20,11 +20,12 @@
 struct process {
 
 	long id;
-	float arrival_time, execution_time, deadline;
+	double arrival_time, execution_time, deadline, quantum, remaining_time;
 	char* name;
 };
-
 typedef struct process process;
+
+
 
 /* 	Variaveis globais. Não sei se todas necessariamente precisam ser globais.
 	Quando terminar a gente olha	*/ 
@@ -32,9 +33,9 @@ typedef struct process process;
 int total_processes = 0;
 process processes [MAX];
 pthread_t threads[MAX];
-long thread_ids[MAX];
 clock_t initial_time;
 double global_time = 0;
+process processes_queue[MAX];
 
 pthread_mutex_t mutex;
 
@@ -49,33 +50,7 @@ int compare_processes_qsort(const void * a, const void * b){
 void list_processes(){
 	int i;
 	for (i = 0; i < total_processes; i++)
-		printf("|ID: %ld | |t0: %f | |name: %s | |dt: %f | |dl:%f |\n", processes[i].id, processes[i].arrival_time, processes[i].name, processes[i].execution_time, processes[i].deadline);
-}
-
-/* 	Função que executa o que realmente deve ser feito no EP (O que terá que ser feito nas threads).
-	Depois muda o nome e pah */
-
-void *execute_process(void * thread_id){
-	long id = (long) thread_id;
-	clock_t start, tick;
-
-	pthread_mutex_lock(&mutex);
-	double cronometer_a = (- DBL_MIN), cronometer_b;	//	Cronometros para cronometrar o tempo total que o programa esta rodando.
-	start = tick = clock();								//	Começa de - DBL_MIN devido a chegada do processo poder ser 0
-	while ((cronometer_a < processes[id].execution_time) && (global_time < processes[id].deadline)){	//	Tempo do processo / deadline
-		cronometer_b = difftime(tick, start) / CLOCKS_PER_SEC;
-		tick = clock();
-		cronometer_a = difftime(tick, start) / CLOCKS_PER_SEC;
-		global_time = global_time + (cronometer_a - cronometer_b);	//	Computa quanto tempo passou em um "tick" do clock
-	}
-	printf("Current CPU: %d\n", sched_getcpu());	// Deixa isso aqui. Tem que ter no relatorio, e não posso esquecer desse comando
-	if ((global_time) < processes[id].deadline)
-		printf("%s rodou um tempo total de %lf segundos.\n", processes[id].name, cronometer_a);
-	else
-		printf("%s parou no deadline de %lf segundos.\n", processes[id].name, processes[id].deadline);
-	pthread_mutex_unlock(&mutex);
-
-	return NULL;
+		printf("|ID: %ld | |t0: %lf | |name: %s | |dt: %lf | |dl:%lf |\n", processes[i].id, processes[i].arrival_time, processes[i].name, processes[i].execution_time, processes[i].deadline);
 }
 
 /*	Recebe o arquivo com os valores de t0, name, dt e deadline, e cria um vetor
@@ -86,8 +61,12 @@ void read_file(char ** argv){
 	FILE *fp;
 	char name[255];
 	fp = fopen(argv[1], "r");
+	if (fp == NULL) {
+      printf ("\nArquivo %s não encontrado.\n", argv[1]);
+      exit (EXIT_FAILURE);
+   	}
 	while(!feof(fp)){
-		if (fscanf(fp, "%f %s %f %f\n", 
+		if (fscanf(fp, "%lf %s %lf %lf\n", 
 		&processes[total_processes].arrival_time, 
 		name, 
 		&processes[total_processes].execution_time, 
@@ -99,9 +78,37 @@ void read_file(char ** argv){
 		processes[total_processes].name = malloc(MAX_STRING_LENGTH * sizeof (char));
 		strcpy(processes[total_processes].name, name);
 		processes[total_processes].id = total_processes;
+		processes[total_processes].quantum = 1;
+		processes[total_processes].remaining_time = processes[total_processes].execution_time;
 		total_processes++;
 	}
 	fclose(fp);
+}
+
+/* 	Função que executa o que realmente deve ser feito no EP (O que terá que ser feito nas threads).
+	Depois muda o nome e pah */
+
+void *run_thread_FCFS(void * thread_id){
+	long id = (long) thread_id;
+	clock_t start, tick;
+
+	pthread_mutex_lock(&mutex);
+	double cronometer_a = 1 / (- DBL_MIN), cronometer_b;	//	Cronometros para cronometrar o tempo total que o programa esta rodando.
+	start = tick = clock();								//	Começa de - DBL_MIN devido a chegada do processo poder ser 0
+	while ((cronometer_a < processes[id].execution_time) && (global_time < processes[id].deadline)){	//	Tempo do processo / deadline
+		cronometer_b = difftime(tick, start) / CLOCKS_PER_SEC;
+		tick = clock();
+		cronometer_a = difftime(tick, start) / CLOCKS_PER_SEC;
+		global_time = global_time + (cronometer_a - cronometer_b);	//	Computa quanto tempo passou em um "tick" do clock
+		processes[id].remaining_time = processes[id].remaining_time - (cronometer_a - cronometer_b);
+	}
+	/*printf("Current CPU: %d\n", sched_getcpu());*/	// Deixa isso aqui. Tem que ter no relatorio, e não posso esquecer desse comando
+	if ((global_time) < processes[id].deadline)
+		printf("%s rodou um tempo total de %lf segundos.\n", processes[id].name, cronometer_a);
+	else
+		printf("%s parou no deadline de %lf segundos.\n", processes[id].name, processes[id].deadline);
+	pthread_mutex_unlock(&mutex);
+	return NULL;
 }
 
 void * FCFS (){
@@ -111,7 +118,63 @@ void * FCFS (){
 	while(i < total_processes){														//	outros tenha que fazer outra função de comparação
 		start_time = (difftime(clock(), initial_time) / CLOCKS_PER_SEC);
 		if ((start_time - processes[i].arrival_time) > (1 / CLOCKS_PER_SEC)){		//	Espera até que um processo esteja disponível. Faz isso
-			assert (0 == pthread_create(&threads[processes[i].id], NULL, execute_process, (void *) (long) i));
+			assert (0 == pthread_create(&threads[i], NULL, run_thread_FCFS, (void *) (long) i));
+			i++;																	//	ate que todos os processos estejam em threads
+		}
+	}
+	return NULL;
+}
+
+void insert_queue(process p, int n){
+	process temp;
+	processes_queue[n] = p;
+	int i = n - 1;
+	int j = n;
+	pthread_mutex_lock(&mutex);
+	while (i >= 0 && processes_queue[i].remaining_time > processes_queue[j].remaining_time && processes_queue[i].remaining_time > 0){
+		temp = processes_queue[i];
+		processes_queue[i] = processes_queue[j];
+		processes_queue[j] = temp;
+		i--;
+		j--;
+	}
+	pthread_mutex_unlock(&mutex);
+}
+
+void * run_thread_SRTN(void * thread_id){
+	long id = (long) thread_id;
+	clock_t start, tick;
+
+	pthread_mutex_lock(&mutex);
+
+	double cronometer_a = 1 / (- DBL_MIN), cronometer_b;	//	Cronometros para cronometrar o tempo total que o programa esta rodando.
+	start = tick = clock();								//	Começa de - DBL_MIN devido a chegada do processo poder ser 0
+	printf("%s comecou a rodar...\n", processes[id].name);
+	while ((cronometer_a < processes[id].execution_time) && (global_time < processes[id].deadline)){	//	Tempo do processo / deadline
+			cronometer_b = difftime(tick, start) / CLOCKS_PER_SEC;
+			tick = clock();
+			cronometer_a = difftime(tick, start) / CLOCKS_PER_SEC;
+			global_time = global_time + (cronometer_a - cronometer_b);	//	Computa quanto tempo passou em um "tick" do clock
+			processes_queue[id].remaining_time = processes_queue[id].remaining_time - (cronometer_a - cronometer_b);
+	}
+	/*printf("Current CPU: %d\n", sched_getcpu());*/	// Deixa isso aqui. Tem que ter no relatorio, e não posso esquecer desse comando
+	if ((global_time) < processes[id].deadline)
+		printf("%s rodou um tempo total de %lf segundos.\n", processes[id].name, cronometer_a);
+	else
+		printf("%s parou no deadline de %lf segundos.\n", processes[id].name, processes[id].deadline);
+	pthread_mutex_unlock(&mutex);
+	return NULL;
+}
+
+void * SRTN (){
+	int i = 0;
+	double start_time;
+	qsort(processes, total_processes, sizeof(process), compare_processes_qsort);	//	qsort de acordo com a ordem de chegada. Talvez pros
+	while(i < total_processes){														//	outros tenha que fazer outra função de comparação
+		start_time = (difftime(clock(), initial_time) / CLOCKS_PER_SEC);
+		if ((start_time - processes[i].arrival_time) > (1 / CLOCKS_PER_SEC)){		//	Espera até que um processo esteja disponível. Faz isso	
+			insert_queue(processes[i], i);
+			assert (0 == pthread_create(&threads[i], NULL, run_thread_SRTN, (void *) (long) i));
 			i++;																	//	ate que todos os processos estejam em threads
 		}
 	}
@@ -123,11 +186,27 @@ int main(int argc, char** argv){
 	initial_time = clock();
 	pthread_mutex_init(&mutex, NULL);
 	read_file(argv);
-	FCFS();
+	SRTN();
 	for (i = 0; i < total_processes; i++)
 		assert(0 == pthread_join(threads[i], NULL));	//	Comando pra esperar as threads finalizarem
+	for (i = 0; i < total_processes; i++)
+		printf("ID: %d 	PROCESSO:	%ld 	remaining_time: %lf\n", i, processes_queue[i].id, processes_queue[i].remaining_time);
 	pthread_exit(NULL);
 	pthread_mutex_destroy(&mutex);
 	return 0;
 
 }
+
+/* 	PRINTS:
+	for (i = 0; i < total_processes; i++)
+		printf("ID: %d 	PROCESSO:	%ld 	remaining_time: %lf\n", i, processes[i].id, processes[i].remaining_time);
+	for (i = 0; i < total_processes; i++)
+		printf("ID: %d 	PROCESSO:	%ld 	remaining_time: %lf\n", i, processes[i].id, processes[i].remaining_time);
+	for (int j = 0; j < i; j++)
+		printf("ID: %d 	PROCESSO:	%ld 	remaining_time: %lf\n", j, processes_queue[j].id, processes_queue[j].remaining_time);
+	printf("%s sendo inserido na fila\n", processes[i].name);
+		printf("%s terminou de rodar...\n", processes[id].name);
+					printf("HOLDING THREAD %ld...\n", id);
+						printf("GLOBAL TIME: %lf\n", global_time);
+
+*/
