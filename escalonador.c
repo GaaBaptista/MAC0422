936e2a93f380ecef52b2b727f82ca0fc622c751d
hassakura <helio.assakura@gmail.com>
2016-09-11 21:34:12 -0300

    ggwp

--- escalonador.c
+++ escalonador.c
@@ -9,7 +9,6 @@
 #include <assert.h>
 #include <sched.h>
 #include <float.h>
-#include <semaphore.h>
 #include <time.h>
 
 #define MAX 10000
@@ -47,17 +46,14 @@ FILE *file;
 
 int sched_getcpu(); //	Frescura do compilador
 
+/* Função usada pelo qsort para ordenação dos processos (pela ordem de chegada) */
+
 int compare_processes_qsort(const void * a, const void * b){
 	return ((*(process *)a).arrival_time - (*(process *)b).arrival_time);
 }
 
-/* 	Printa os processos, só pra ver se foi lido corretamente do arquivo	*/
-
-void list_processes(){
-	int i;
-	for (i = 0; i < total_processes; i++)
-		printf("|ID: %ld | |t0: %lf | |name: %s | |dt: %lf | |dl:%lf |\n", processes[i].id, processes[i].arrival_time, processes[i].name, processes[i].execution_time, processes[i].deadline);
-}
+/*	Cálculo do tempo corrido. Tivemos muita dificuldade em usar clock().
+	Gastamos bastante tempo nisso. 	*/
 
 static double TimeSpecToSeconds(struct timespec* ts)
 {
@@ -97,8 +93,8 @@ void read_file(char ** argv){
 	fclose(fp);
 }
 
-/* 	Função que executa o que realmente deve ser feito no EP (O que terá que ser feito nas threads).
-	Depois muda o nome e pah */
+/*	Simulação da execução do processo. Para o FCFS, não há a ordenação por tempo
+	restante, mas apenas os processos por ordem de chegada	*/
 
 void *run_thread_FCFS(void * thread_id){
 	long id = (long) thread_id;
@@ -108,33 +104,34 @@ void *run_thread_FCFS(void * thread_id){
 	CPU = sched_getcpu();
 	if (wf) fprintf(stderr, "Processo %s usa a CPU %d\n", processes[id].name, CPU);
 	pthread_mutex_lock(&mutex);
-	while (processes[id].remaining_time > 0.0){	//	Tempo do processo / deadline
+	while (processes[id].remaining_time > 0.0){	// Processo ainda não acabou
 		clock_gettime(CLOCK_MONOTONIC, &curr_time);
-		if (processes[id].run_flag == 1){
+		if (processes[id].run_flag == 1)	//Se for a vez dele executar, executa
 			processes[id].remaining_time -= (TimeSpecToSeconds(&curr_time) - TimeSpecToSeconds(&st_time));
-		}
+		/* Contamos quanto tempo o processo permaneceu executando	*/
 		st_time = curr_time;
 	}
 	pthread_mutex_unlock(&mutex);
 	clock_gettime(CLOCK_MONOTONIC, &curr_time);
-	double tf = (TimeSpecToSeconds(&curr_time) - TimeSpecToSeconds(&initial_time));
+	double tf = (TimeSpecToSeconds(&curr_time) - TimeSpecToSeconds(&initial_time));	/*	Calculo do tempo gasto ate a conclusao	*/
 	fprintf(file, "%s %lf %lf\n",processes[id].name, tf, tf - processes[id].arrival_time);
 	if (wf) {
 		fprintf(stderr, "Processo %s liberou a CPU %d\n", processes[id].name, CPU);
 		fprintf(stderr, "Linha da saída: %d\n", line++);
 	}
-	/*printf("Current CPU: %d\n", sched_getcpu());*/	// Deixa isso aqui. Tem que ter no relatorio, e não posso esquecer desse comando
 	return NULL;
 }
 
 void * FCFS (){
 	int i = 0;
 	double tick_time;
-	qsort(processes, total_processes, sizeof(process), compare_processes_qsort);	//	qsort de acordo com a ordem de chegada. Talvez pros
-	while(i < total_processes){														//	outros tenha que fazer outra função de comparação
+	// Ordenamos pelo tempo de chegada, criando a fila de processos a ser executada
+	qsort(processes, total_processes, sizeof(process), compare_processes_qsort);	
+	while(i < total_processes){		//	Enquanto ele não criou uma thread pra cada processo												
 		clock_gettime(CLOCK_MONOTONIC, &end);
 		tick_time = (TimeSpecToSeconds(&end) - TimeSpecToSeconds(&initial_time));
-		for ( ; tick_time > processes[i].arrival_time && i < total_processes; ){		//	Espera até que um processo esteja disponível. Faz isso	
+		/*	Verificamos se o próximo processo "já chegou". Se chegou, criamos a thread pra ele	*/
+		for ( ; tick_time > processes[i].arrival_time && i < total_processes; ){
 			if (wf)
 				fprintf(stderr, "Processo %s chegou no sistema. Linha %d\n", processes[i].name, i);
 			processes[i].run_flag = 1;
@@ -146,12 +143,17 @@ void * FCFS (){
 	return NULL;
 }
 
+/*	Função que recebe a quantidade de processos lida até agora e ordena de acordo com o menor tempo restante
+	até a conclusão de cada processo 	*/
+
 void  sort_queue (int n){
 	int current = n, previous = n - 1;
 	process temp;
 	pthread_mutex_lock(&mutex);
+	/*	Inicialmente, ninguém pode executar. 	*/
 	for (int i = 0; i <= n; i++)
 		processes[i].run_flag = 0;
+	/* Já que um processo novo chegou, decidimos em que lugar deve ficar. Usamos algo como um mini bubblesort	*/
 	while (n && previous >= 0 && processes[previous].remaining_time > processes[current].remaining_time){
 		temp = processes[previous];
 		processes[previous] = processes[current];
@@ -159,7 +161,9 @@ void  sort_queue (int n){
 		previous--;
 		current--;
 	}
-	if (current == 0) context_change++;
+	if (current == 0) context_change++;	/* 	Se o processo atual chegou até o começo da fila, ele deve ser executado,
+											mantendo em hold o processo anteriormente em execução	*/
+	/*	Como colocamos ele no devido lugar, descartamos os processos que já acabaram de executar	*/
 	for (int i = 0; i <= n; i++)
 		if (processes[i].remaining_time > 0){
 			processes[i].run_flag = 1;
@@ -168,13 +172,14 @@ void  sort_queue (int n){
 	pthread_mutex_unlock(&mutex);
 }
 
+
 void * run_thread_SRTN(void * thread_id){
 	long id = (long) thread_id;
 	struct timespec st_time, curr_time;
 	clock_gettime(CLOCK_MONOTONIC, &st_time);
 	int CPU;
 	CPU = sched_getcpu();
-	while (processes[id].remaining_time > 0.0){	//	Tempo do processo / deadline
+	while (processes[id].remaining_time > 0.0){
 		pthread_mutex_lock(&mutex);
 		clock_gettime(CLOCK_MONOTONIC, &curr_time);
 		if (processes[id].run_flag == 1)
@@ -189,26 +194,32 @@ void * run_thread_SRTN(void * thread_id){
 		fprintf(stderr, "Processo %s liberou a CPU %d\n", processes[id].name, CPU);
 		fprintf(stderr, "Linha da saída: %d\n", line++);
 	}
-	/*printf("Current CPU: %d\n", sched_getcpu());*/	// Deixa isso aqui. Tem que ter no relatorio, e não posso esquecer desse comando
 	
 	return NULL;
 }
 
 void * SRTN (){
+
+	/* 	Semelhante ao FCFS, ordenamos os processos por ordem de chegada. A diferença é que devemos
+		executar os processos com tempo de resolução mais curto primeiro. Isso será verificado
+		cada vez que um novo processo entrar. Ao final da leitura de todos os processos, podemos
+		executar em ordem, pois estarão ordenados.	*/
+
 	int i = 0;
 	double tick_time;
-	qsort(processes, total_processes, sizeof(process), compare_processes_qsort);	//	qsort de acordo com a ordem de chegada. Talvez pros
-	while(i < total_processes){														//	outros tenha que fazer outra função de comparação
+	qsort(processes, total_processes, sizeof(process), compare_processes_qsort);	
+	while(i < total_processes){														
 		clock_gettime(CLOCK_MONOTONIC, &end);
 		tick_time = (TimeSpecToSeconds(&end) - TimeSpecToSeconds(&initial_time));
-		for ( ; tick_time > processes[i].arrival_time && i < total_processes; ){		//	Espera até que um processo esteja disponível. Faz isso	
+		for ( ; tick_time > processes[i].arrival_time && i < total_processes; ){
 			if (wf)
 				fprintf(stderr, "Processo %s chegou no sistema. Linha %d\n", processes[i].name, i);
-			sort_queue(i);
+			sort_queue(i);	// Chegou um processo, colocamos ele na ordem correta
 			pthread_create(&threads[i], NULL, run_thread_SRTN, (void *) (long) i);
 			i++;
 		}
 	}
+	/*	Todos processos lidos, temos que terminar de executar o resto deles	*/
 	for (int j = 0; j < total_processes; j++)
 		processes[j].run_flag = 0;
 	for (int j = 0; j < total_processes; j++)
