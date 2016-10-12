71ab53118faa05c6f5ef2a88baf52428fcc43fcf
hassakura <helio.assakura@gmail.com>
2016-10-12 13:42:48 -0300

    falta quebrar

--- EP2/ep2so.c
+++ EP2/ep2so.c
@@ -12,7 +12,7 @@
 #include <time.h>
 
 #define team_size 4
-#define track_size 20
+#define track_size 30
 
 struct cyclist{	
     pthread_t thread_cyclist;
@@ -30,6 +30,7 @@ typedef struct cyclist cyclist;
 cyclist cyclists [2 * team_size];
 pthread_barrier_t barrier;
 long track [track_size][2];
+long leader_0, leader_1;
 pthread_mutex_t mutex;
 
 /* Contagem de tempo (nao necessario) */
@@ -51,12 +52,11 @@ void create_cyclists(long id){
 	if (id < team_size){
 		cyclists[id].team_ID = 0;
 		cyclists[id].pos = 0;
-		cyclists[id].start_pos = 0;
+		
 	}
 	else{
 		cyclists[id].team_ID = 1;
 		cyclists[id].pos = track_size / 2;
-		cyclists[id].start_pos = track_size / 2;
 	}
 }
 
@@ -71,6 +71,13 @@ long start_position(long id){
 	int i = 0;
 	while (track[cyclists[id].pos + i][0] != -1) i++;
 	track[cyclists[id].pos + i][0] = id;
+	cyclists[id].start_pos = cyclists[id].pos + i;
+	if (i == 1){
+		if (cyclists[id].team_ID == 0)
+			leader_0 = id;
+		else 
+			leader_1 = id;
+	}
 	return (cyclists[id].pos + i);
 }
 
@@ -87,6 +94,7 @@ void print_track(){
 	a corrida. Ainda falta fazer a segunda parte */
 
 void * run_const(void * argument){
+	int s;
 	long thread_id, start;
 	thread_id = (long) argument;
 	int track_side = 0;
@@ -99,14 +107,12 @@ void * run_const(void * argument){
 	pthread_barrier_wait(&barrier);
 
 	/* Todos os ciclistas estao a postos, prontos para comecar a corrida */
-
-	while (cyclists[thread_id].completed_laps < 4){
-		
+	while (cyclists[leader_0].completed_laps < 4 || cyclists[leader_1].completed_laps < 4){
 		/* Atualizacao da posicao do ciclista depois de 60ms. Usamos mod para facilitar a atualizacao
 			da posicao de cada um. Ainda precisamos arrumar isso, colocar a condicao de quebra, e o modo
 			debug. Falta coisa pra coroi */
-
-		pthread_mutex_lock(&mutex);	
+		s = pthread_barrier_wait(&barrier);
+		pthread_mutex_lock(&mutex);
 
 		/* Apaga a ultima posicao dele, dependendo de que pista ele veio */
 		if (track_side == 0)
@@ -114,6 +120,7 @@ void * run_const(void * argument){
 		else
 		    track[(cyclists[thread_id].pos) % track_size][1] = -1;
 		/* Avança o ciclista pra uma das pistas vagas */
+		
 		cyclists[thread_id].pos += 1;
 		if (track[cyclists[thread_id].pos % track_size][0] == -1){
 		    track[cyclists[thread_id].pos % track_size][0] = thread_id;
@@ -123,21 +130,21 @@ void * run_const(void * argument){
 		    track[cyclists[thread_id].pos % track_size][1] = thread_id;
 			track_side = 1;
 		}
-		print_track();
-		pthread_mutex_unlock(&mutex);
-		pthread_barrier_wait(&barrier);
-
-		/* Todos andaram e tem suas posicoes. Printamos pra saber como esta a fila */
-
-		print_cyclist(thread_id);
-
-		nsleep(600000);
 
 		/*	verificamos se foi completada uma volta */
+
 		if ((cyclists[thread_id].pos % track_size) == cyclists[thread_id].start_pos)
 			cyclists[thread_id].completed_laps += 1;
+
+		pthread_mutex_unlock(&mutex);
+		/* Todos andaram e tem suas posicoes. Printamos pra saber como esta a fila */
+		
+		nsleep(200000);
+		if (s == PTHREAD_BARRIER_SERIAL_THREAD)
+			print_track();
+		
+		
 	}
-	printf("Ciclista %ld acabou\n", thread_id);
 	return NULL;
 }
 
@@ -158,6 +165,7 @@ int main(int argc, char** argv){
 	for (id = 0; id < (2 * team_size); id++){
 		pthread_join(cyclists[id].thread_cyclist, NULL);
 	}
+	printf("FINAL: \n"); print_track();
 	return 0;
 }
 
